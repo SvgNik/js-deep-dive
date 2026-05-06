1419adef235123fe027779b484cde84d9d987f93
Nik <kozaknik911@gmail.com>
2026-05-06 20:02:21 +0200

    feat(js-01): add user data variables practice

---
+++ 01-variables-and-types/01-user-data.js
@@ -0,0 +1,12 @@
+const userName = 'Alex';
+const age = 25;
+const city = 'Kyiv';
+const isLoggedIn = true;
+let counter = 0;
+counter = counter + 1;
+
+console.log(userName);
+console.log(age);
+console.log(city);
+console.log(isLoggedIn);
+console.log(counter);
