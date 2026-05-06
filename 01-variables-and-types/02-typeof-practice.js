93add2e8313b410d22494cdbfc594d9b7209964a
Nik <kozaknik911@gmail.com>
2026-05-06 20:11:46 +0200

    feat(js-01): add typeof practice

---
+++ 01-variables-and-types/02-typeof-practice.js
@@ -0,0 +1,23 @@
+const name = 'Nik';
+console.log(name);
+console.log(typeof name);
+
+const age = 25;
+console.log(age);
+console.log(typeof age);
+
+const isPlayerLog = true;
+console.log(isPlayerLog);
+console.log(typeof isPlayerLog);
+
+const user = null;
+console.log(user);
+console.log(typeof user);
+
+let score;
+console.log(score);
+console.log(typeof score);
+
+const userAge = 10 / 0;
+console.log(userAge);
+console.log(typeof userAge);
