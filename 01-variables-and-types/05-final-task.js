5b73e0e7d892d793a5ad54ea1fe1d6baec018726
Nik <kozaknik911@gmail.com>
2026-05-07 14:24:52 +0200

    feat(js-01): add final task player profile

---
+++ 01-variables-and-types/05-final-task.js
@@ -0,0 +1,23 @@
+const userName = "Name"; // Потому что просто имя
+console.log(userName);
+console.log(typeof userName);
+
+let userLevel = 1; // Потому что левел будет изменяться
+console.log(userLevel);
+console.log(typeof userLevel);
+
+let numberOfPoints = 0; // номер будет изменяться
+console.log(numberOfPoints);
+console.log(typeof numberOfPoints);
+
+let userOnline = true; // let - потому что он будет выходить и заходить то есть true - false
+console.log(userOnline);
+console.log(typeof userOnline);
+
+let selectedCharacter = null; // из за того, что позже значение измениться let потому что когда то персонаж будет выбран
+console.log(selectedCharacter);
+console.log(typeof selectedCharacter);
+
+let lastCompleted = undefined; // из за того, что позже значение измениться let потому что когда то миссия будет пройдена
+console.log(lastCompleted);
+console.log(typeof lastCompleted);
