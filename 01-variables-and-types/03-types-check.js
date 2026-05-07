661a0b0ce3aad89c3ca7ddb77d8e9c9b09595a3c
Nik <kozaknik911@gmail.com>
2026-05-07 13:58:26 +0200

    feat(js-01): add types check practice

---
+++ 01-variables-and-types/03-types-check.js
@@ -0,0 +1,23 @@
+const a = "42";
+console.log(a);
+console.log(typeof a); // string
+
+const b = 42;
+console.log(b);
+console.log(typeof b); // number
+
+const c = false;
+console.log(c);
+console.log(typeof c); // boolean
+
+const d = null;
+console.log(d);
+console.log(typeof d); // object
+
+const e = undefined;
+console.log(e);
+console.log(typeof e); // undefined
+
+const f = NaN;
+console.log(f);
+console.log(typeof f); // number
