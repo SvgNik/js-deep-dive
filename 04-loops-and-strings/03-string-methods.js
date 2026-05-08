e9fd4b4ad199a47eb7d1d93cad6ea48bc4e50ea3
Nik <kozaknik911@gmail.com>
2026-05-08 16:58:47 +0200

    feat(js-04): add string methods practice

---
+++ 04-loops-and-strings/03-string-methods.js
@@ -0,0 +1,11 @@
+const coder = ' JavaScript Developer ';
+console.log(coder.trim().toLowerCase());
+
+const developer = 'front-end developer';
+console.log(developer.includes('developer'));
+
+const string = 'Hello, World!';
+console.log(string.slice(7, 12));
+
+const email = 'john.doe@gmail.com';
+console.log(email.indexOf('@'))
