6bbdb501321eda14c659324d8b9d6bd2de8b52e2
Nik <kozaknik911@gmail.com>
2026-05-08 14:22:36 +0200

    feat(js-04): add break and continue practice

---
+++ 04-loops-and-strings/02-break-continue.js
@@ -0,0 +1,12 @@
+// for (let i = 1; i <= 20; i++) {
+//   if (i % 3 === 0) continue;
+//   console.log(i)
+// }
+
+let sum = 0;
+
+for (let i = 1; i <= 100; i++) {
+  sum += i;
+  if (sum > 50) break;
+  console.log(i);
+}
