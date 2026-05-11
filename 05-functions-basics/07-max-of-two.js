9c46863be4c73548c92f3882f63a0d86b42ec35e
Nik <kozaknik911@gmail.com>
2026-05-11 20:36:32 +0200

    feat(js-05): add max of two numbers function

---
+++ 05-functions-basics/07-max-of-two.js
@@ -0,0 +1,12 @@
+function maxOfTwo (a, b) {
+  if (a > b) {
+    return a
+  } else {
+    return b
+  }
+}
+
+console.log(maxOfTwo(5, 12));
+console.log(maxOfTwo(7, 42));
+console.log(maxOfTwo(18, 18));
+console.log(maxOfTwo(21, 12));
