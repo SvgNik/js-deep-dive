69e6b4165ef88d6dfb7a5d632fb73021448dacb4
Nik <kozaknik911@gmail.com>
2026-05-11 20:25:08 +0200

    feat(js-05): add even/odd check function

---
+++ 05-functions-basics/06-even-odd.js
@@ -0,0 +1,13 @@
+function isEven(number) {
+    if (number % 2 === 0) {
+      return true;
+    }
+    else {
+      return false;
+    }
+}
+
+console.log(isEven(8));
+console.log(isEven(13));
+console.log(isEven(0));
+console.log(isEven(-4));
