4c4a3a3490c47741eaa406a2308cb15bd1945900
Nik <kozaknik911@gmail.com>
2026-05-11 19:40:31 +0200

    feat(js-05): add age validation function

---
+++ 05-functions-basics/03-validate-age.js
@@ -0,0 +1,11 @@
+function isAdult(age) {
+  if (age >= 18) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isAdult(15));
+console.log(isAdult(18));
+console.log(isAdult(20));
