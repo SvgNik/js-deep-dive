851c5b0aff39d7d2b9eceb40e27d59f5f55b3498
Nik <kozaknik911@gmail.com>
2026-05-21 19:07:40 +0200

    feat(js-02): add strings review task

---
+++ 02-strings-and-operators/review-02.js
@@ -0,0 +1,10 @@
+const raw = "  john.doe@example.com  ";
+const result = raw.trim();
+const upper = result.toUpperCase();
+
+
+console.log(result);
+console.log(upper);
+console.log(upper.includes('EXAMPLE'));
+console.log(upper.length);
+console.log(upper[0]);
