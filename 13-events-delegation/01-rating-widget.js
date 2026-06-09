150e72bf8de311ded38b2f982968def682fdda47
Nik <kozaknik911@gmail.com>
2026-06-09 15:20:45 +0200

    feat(js-13): add star rating widget with event.target and dataset

--- 13-events-delegation/01-rating-widget.js
+++ 13-events-delegation/01-rating-widget.js
@@ -1,9 +1,9 @@
 const buttons = document.querySelectorAll(".star");
-const result = document.querySelector(".rating-result")
+const result = document.querySelector(".rating-result");
 
 buttons.forEach((btn) => {
   btn.addEventListener("click", (event) => {
     const value = Number(event.target.dataset.value);
     result.textContent = `Оценка: ${value} из 5`;
-  })
+  });
 });
