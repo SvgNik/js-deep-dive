2bf811387202b0faa1fef4fd996cf4c1c676dcc1
Nik <kozaknik911@gmail.com>
2026-06-29 21:02:18 +0200

    wip(js-16): add users dashboard — loading + fetch + render cards

---
+++ 16-http-fetch/03-users-dashboard/03-users-dashboard.js
@@ -0,0 +1,32 @@
+const status = document.querySelector("#status");
+const stats = document.querySelector("#stats");
+const usersCard = document.querySelector("#usersCard");
+const search = document.querySelector("#search");
+
+status.textContent = "...Загрузка";
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`Ошибка, статус: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((users) => {
+    status.textContent = "";
+    users.forEach((user) => {
+      const card = document.createElement("div");
+      const userName = document.createElement("p");
+      const email = document.createElement("p");
+      const addressCity = document.createElement("p");
+      const companyName = document.createElement("p");
+
+      userName.textContent = user.name;
+      email.textContent = user.email;
+      addressCity.textContent = user.address.city;
+      companyName.textContent = user.company.name;
+      card.append(userName, email, addressCity, companyName);
+      usersCard.append(card);
+    });
+  })
+  .catch((error) => console.log(error.message));
